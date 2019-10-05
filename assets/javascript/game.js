340a3f7fe0e2565c1c7be4eda4f68fad2b2d20bd
Angelo Capozzi <acap613@gmail.com>
2019-10-05 17:32:47 -0400

    pushing the work i ahve so far...in case I mess it up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,81 @@
-// Global  Scope variables
-var baseAttack = 0; // original attack strength
-var player; // holds the player Object
-var defender; // holds the current defender Object
-var charArray = []; // array that stores the game characters (Objects)
-var playerSelected = false; // flag to mark if we picked a player yet
-var defenderSelected = false; // flag to mark if we picked a defender
+    // start the game:
+  
+       
+       // Global  Scope variables
+        
+        // hero selected and cooresponding stats, Health and attackPower only,
+        var attacker;
+        var attackerHealth;
+        var attackerAttack;
+        // defender selected and cooresponding stats, note that defenderAttack is actually counterAttackPower...
+        var defender; 
+        var defenderHealth;
+        var defenderAttack; 
+        // some variables to help define new classes maybe
+        var name;
+        var fullname;
+        var pickAttacker;
+        var pickDefender;
+        var myAttacker = "";
+        var myDefender = "";
 
-// lets make the characters
-var Luke = {
-    health: 150,
-    attackPower: 20,
-    counterAttackPower: 15,
-}
-var Vader = {
-    health: 200,
-    attackPower: 14,
-    counterAttackPower: 25,
-}
-var Solo = {
-    health: 120,
-    attack: 30,
-    counterAttackPower: 12,
-}
-var Fett = {
-    health: 220,
-    attackPower: 18,
-    counterAttackPower: 18,
-}
-// lets make some logic:
+        
+        // empty array for functions if need, its defined.
+        var characterArray = []; 
+        // array that already contains all characters.
+        var characterPick = [Luke, Vader, Solo, Fett,];    
+
+
+
+     function reset() {
+
+        // lets make the characters
+        var characters = {
+                Luke: {
+                    name: 'Luke',
+                    fullname: 'Luke Skywalker',
+                    health: 150,
+                    attackPower: 20,
+                    counterAttackPower: 15,
+                    image: '<img src="assets/images/Luke.jpg" class="image">'
+                },
+                Vader: {
+                    name: 'Vader',
+                    fullname: 'Darth Vader',
+                    health: 200,
+                    attackPower: 14,
+                    counterAttackPower: 25,
+                    image: '<img src="./assets/images/Vader.jpg" class="image">'
+                },
+                Solo: {
+                    name: 'Solo',
+                    fullname: 'Han Solo',
+                    health: 120,
+                    attack: 30,
+                    counterAttackPower: 12,
+                    image: '<img src="./assets/images/Solo.jpg" class="image">'
+                },
+                Fett: {
+                    name: 'Fett',
+                    fullname: 'Boba Fett',
+                    health: 220,
+                    attackPower: 18,
+                    counterAttackPower: 18,
+                    image: '<img src="./assets/images/Solo.jpg" class="image">'
+                },
+            }
+        // lets make the game work:
+        // starting with character selection
+        function startGame() {
+    
+            createCharacters(characterPick);
+            pickAttacker();
+            pickDefender();
+           
+        }
+        // need to move the character selected as "player" div move into "attacker" div, onClckEvent?
+       
+        // after which we need to move the character selected as "defender" div into the area called "defender" div:
+
+// <---close--->
+
